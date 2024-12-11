47b5650135d571b392208894463b5625e9c166e6
Devitz <danieladevitz@gmail.com>
2024-12-11 19:33:07 -0300

    Fazendo os primeiros testes de movimentacao

--- Tela.cs
+++ Tela.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -29,11 +31,20 @@ namespace xadrez_console
             Console.WriteLine("  A B C D E F G H");
         }
 
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+
+            return new PosicaoXadrez(coluna, linha);
+        }
+
         public static void imprimirPeca(Peca peca)
         {
             if (peca.cor == Cor.Branca)
             {
-                Console.WriteLine(peca);
+                Console.Write(peca);
             }
             else
             {
